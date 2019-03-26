d02b681ca6855182f4bb31c58cc58666f91f556b
Oleh Tsivan <oleh.tsivan@sysgears.com>
2019-03-26 15:35:57 +0200

    Remove logging configuration

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,21 +2,26 @@ import akka.actor.ActorSystem
 import com.google.inject.Guice
 import modules.{AkkaModule, ConfigModule, DBModule}
 import services.KafkaServices
-import services.github.GitHubRepositoryService
-import utils.LoggingConfig
+import services.github.GitHubProjectService
 
-object Main extends App with LoggingConfig with KafkaServices {
+import scala.concurrent.ExecutionContext
+
+object Main extends App with KafkaServices {
 
   private val body = "{ \"query\": \"query { search(query: Java, type: REPOSITORY, first: 1) { pageInfo { hasNextPage startCursor endCursor } edges { node { ... on Repository { id name description createdAt stargazers { totalCount } forkCount updatedAt dependencyGraphManifests { totalCount nodes { dependencies { edges { node { packageName requirements } } } } } } } } } } \"}"
   private val injector = Guice.createInjector(new ConfigModule, new AkkaModule, new DBModule)
 
-  val gitHubRepositoryService: GitHubRepositoryService = injector.getInstance(classOf[GitHubRepositoryService])
-  gitHubRepositoryService.fetchRepositoriesWithGraphQL(body, 10, 5)
+  implicit val ec = injector.getInstance(classOf[ExecutionContext])
 
-//  test drive of transferring data from mongodb to kafka through spark
-//  val resultDataFrame = readFromMongoBySpark()
-//  resultDataFrame._1.forEach(t => sendToKafka("test-data-topic", t._id, t.toString))
-//  resultDataFrame._2.stop
+  val gitHubRepositoryService: GitHubProjectService = injector.getInstance(classOf[GitHubProjectService])
+  gitHubRepositoryService.fetchRepositoriesWithGraphQL(body, 10, 5).onComplete {
+    _ => injector.getInstance(classOf[ActorSystem]).terminate
+  }
 
-  injector.getInstance(classOf[ActorSystem]).terminate
+  //  test drive of transferring data from mongodb to kafka through spark
+  //  val resultDataFrame = readFromMongoBySpark()
+  //  resultDataFrame._1.forEach(t => sendToKafka("test-data-topic", t._id, t.toString))
+  //  resultDataFrame._2.stop
+
+
 }

cbd87520a7731b539320fae70d32fbb50f8e3c28
Oleh Tsivan <oleh.tsivan@sysgears.com>
2019-03-26 15:30:30 +0200

    Add bind of ReactiveMongo

--- src/main/scala/modules/DBModule.scala
+++ src/main/scala/modules/DBModule.scala
@@ -1,44 +1,21 @@
 package modules
 
 import com.google.inject.Provides
-import com.mongodb.MongoClientSettings
-import com.mongodb.reactivestreams.client.MongoDatabase
-import com.mongodb.connection.ClusterSettings
-import com.mongodb.reactivestreams.client.{MongoClient, MongoClients}
-import com.mongodb.{Block, ServerAddress}
 import com.typesafe.config.Config
 import javax.inject.Singleton
 import net.codingwell.scalaguice.ScalaModule
+import repositories.ReactiveMongo
 
-import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext
 
 class DBModule extends ScalaModule {
 
   @Provides
   @Singleton
-  def mongoDatabase(mongoClient: MongoClient): MongoDatabase = {
-    mongoClient.getDatabase("default")
-  }
-
-  @Provides
-  @Singleton
-  def mongoClient(config: Config): MongoClient = {
-
-    val host = config.getString("mongodb.host")
-    val port = config.getNumber("mongodb.port").intValue
-
-    val clusterSettings: ClusterSettings = ClusterSettings.builder().hosts {
-      List(new ServerAddress(host, port)).asJava
-    }.build
-    val block = new Block[ClusterSettings.Builder] {
-      override def apply(t: ClusterSettings.Builder): Unit = {
-        t.applySettings(clusterSettings)
-      }
-    }
-    val settings = MongoClientSettings
-      .builder
-      .applyToClusterSettings(block)
-      .build
-    MongoClients.create(settings)
+  //todo: add custom execution context
+  def reactiveMongoApi(config: Config)(implicit ec: ExecutionContext): ReactiveMongo = {
+    val uri = config.getString("mongodb.uri")
+    val name = config.getString("mongodb.name")
+    new ReactiveMongo(uri, name)
   }
 }

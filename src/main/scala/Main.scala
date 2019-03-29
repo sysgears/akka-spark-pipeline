e4158d09f30f242aec05001c31fca5b68dfb21f7
Oleh Tsivan <oleh.tsivan@sysgears.com>
2019-03-29 10:36:11 +0200

    Add graph creation into Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,27 +1,22 @@
-import akka.actor.ActorSystem
 import com.google.inject.Guice
 import modules.{AkkaModule, ConfigModule, DBModule}
-import services.KafkaServices
 import services.github.GitHubProjectService
+import services.github.spark.GitHubGraphXService
+import services.spark.SparkMongoService
 
 import scala.concurrent.ExecutionContext
 
-object Main extends App with KafkaServices {
+object Main extends App {
 
   private val body = "{ \"query\": \"query { search(query: Java, type: REPOSITORY, first: 1) { pageInfo { hasNextPage startCursor endCursor } edges { node { ... on Repository { id name description createdAt stargazers { totalCount } forkCount updatedAt dependencyGraphManifests { totalCount nodes { dependencies { edges { node { packageName requirements } } } } } } } } } } \"}"
   private val injector = Guice.createInjector(new ConfigModule, new AkkaModule, new DBModule)
 
   implicit val ec = injector.getInstance(classOf[ExecutionContext])
-
   val gitHubRepositoryService: GitHubProjectService = injector.getInstance(classOf[GitHubProjectService])
   gitHubRepositoryService.fetchRepositoriesWithGraphQL(body, 10, 5).onComplete {
-    _ => injector.getInstance(classOf[ActorSystem]).terminate
+    _ =>
+      val (dataFrame, _) = injector.getInstance(classOf[SparkMongoService]).loadData
+      injector.getInstance(classOf[GitHubGraphXService]).createGraphFrame(dataFrame)
+      //After that when we've created GraphFrame object we can store this graph into Neo4j database.
   }
-
-  //  test drive of transferring data from mongodb to kafka through spark
-  //  val resultDataFrame = readFromMongoBySpark()
-  //  resultDataFrame._1.forEach(t => sendToKafka("test-data-topic", t._id, t.toString))
-  //  resultDataFrame._2.stop
-
-
 }

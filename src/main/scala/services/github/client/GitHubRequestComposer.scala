cf3f1e27c7264e5a4e04d5da76d103d5c2db115b
pavel.grudina <plgrudina@gmail.com>
2019-04-04 17:24:50 +0300

    Added authorization token for Github request

--- src/main/scala/services/github/client/GitHubRequestComposer.scala
+++ src/main/scala/services/github/client/GitHubRequestComposer.scala
@@ -8,7 +8,9 @@ import services.github.client.GitHubRequestComposer._
 object GitHubRequestComposer {
 
   //todo: add personal GitHub token in order to perform GraphQL request to GitHub
-  private val headers = RawHeader("Authorization", "Bearer ") ::
+  private val token = ""
+
+  private val headers = RawHeader("Authorization", "Bearer " + token) ::
     RawHeader("Accept", "application/vnd.github.hawkgirl-preview") :: Nil
   private val uri: Uri = Uri("https://api.github.com/graphql")
   private val httpRequest = HttpRequest(uri = uri, method = HttpMethods.POST).withHeaders(headers)

4f7566f279b3391d8e6dbf52a354a4a482d6886f
pavel.grudina <plgrudina@gmail.com>
2019-04-11 18:00:30 +0300

    Fixed bug with GraphFrame building

--- src/main/scala/services/github/spark/GitHubGraphXService.scala
+++ src/main/scala/services/github/spark/GitHubGraphXService.scala
@@ -1,12 +1,14 @@
 package services.github.spark
 
+import scala.language._
 import com.google.inject.Inject
-import models.{Dependency, GitHubRepository}
-import org.apache.spark.sql.{DataFrame, Encoder, Encoders, Row}
+import models.{Dependency, GitHubRepository, RelationProp}
+import org.apache.spark.sql._
 import org.graphframes.GraphFrame
 import services.spark.SparkMongoService
 
 import scala.collection.mutable
+import scala.util.Random
 
 class GitHubGraphXService @Inject()(sparkMongoService: SparkMongoService) {
 
@@ -20,36 +22,47 @@ class GitHubGraphXService @Inject()(sparkMongoService: SparkMongoService) {
     //todo: move encoders in an external class
     implicit val gitHubRepositoryEncoder: Encoder[GitHubRepository] = Encoders.product[GitHubRepository]
     implicit val dependencyEncoder: Encoder[Dependency] = Encoders.product[Dependency]
+    implicit val relationEncoder: Encoder[RelationProp] = Encoders.product[RelationProp]
     implicit val stringEncoder: Encoder[String] = Encoders.STRING
-    implicit val tupleEncoders: Encoder[(String, String, String)] = Encoders.tuple(Encoders.STRING, Encoders.STRING, Encoders.STRING)
+    implicit val tuple2Encoders: Encoder[(Long, String)] = Encoders.product[(Long, String)]
+    implicit val longEncoders: Encoder[Long] = Encoders.scalaLong
+    implicit val long2Encoders: Encoder[(Long, Long, RelationProp)] = Encoders.product[(Long, Long, RelationProp)]
+    implicit val tuple3Encoders: Encoder[(String, String, String)] = Encoders.tuple(Encoders.STRING, Encoders.STRING, Encoders.STRING)
+
+    val centerVertexID = Math.abs(Random.nextLong())
 
     val projects = dataFrame
       .select("_id", "dependencies")
       .filter(""" size(dependencies) != 0 """)
+      .toDF()
 
-    //Create vertices and edges in order to create a graph structure.
-    val vertices: DataFrame = projects
-      .flatMap(_.getAs[mutable.WrappedArray[Row]]("dependencies").map(_.getAs[String]("packageName")))
-      .distinct
-      .toDF("id")
-    //in order to view a result table use vertices.show()
-    //todo: group by technologies. Must be (src, dst, list of projects ids) instead of (src, dst, project id)
-    val edges: DataFrame = projects.flatMap {
-      row =>
-        val id = row.getAs[String]("_id")
-        val packages = row
-          .getAs[mutable.WrappedArray[Row]]("dependencies")
-          .map(_.getAs[String]("packageName"))
-        foldIntoPairs(packages.toList).map(t => (t._1, t._2, id))
-    }.toDF("src", "dst", "projects")
-    //id order to view a result table use edges.show()
+    val centralVertex = dataFrame
+      .select("name")
+      .filter(""" name == "Java" """)
+      .distinct()
+      .map(name => (centerVertexID, name.getAs[String]("name"))).toDF("id", "package")
+
+    val vertices: DataFrame = {
+      val vertexList = projects
+          .flatMap(_.getAs[mutable.WrappedArray[Row]]("dependencies").map(dependency => {
+              val packageName = Some(dependency.getAs[String]("packageName")).getOrElse("emptyName")
+              (Math.abs(Random.nextLong()), packageName)
+          }))
+          .distinct
+          .toDF("id", "package")
+      vertexList.union(centralVertex)
+    }
+
+    val edges: DataFrame = {
+      val dst = vertices.filter(""" package == "Java" """).distinct().map(_.getAs[Long]("id")).first()
+      vertices.map {
+        ver => {
+          val src = ver.getAs[Long]("id")
+          (src, dst, RelationProp(1))
+        }
+      }.toDF("src", "dst", "relationship")
+    }
 
     GraphFrame(vertices, edges)
-  }
-
-  // transform (a, b, c, d) into ((a, b), (a, c), (a, d), (b, c), (b, d), (c, d))
-  def foldIntoPairs[T](list: List[T]): List[(T, T)] = list match {
-    case head :: tail => tail.map(el => (head, el)) ++ foldIntoPairs(tail)
-    case _ => Nil
   }
 }

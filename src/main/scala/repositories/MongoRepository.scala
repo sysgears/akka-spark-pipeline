ecc8a2671898b0285f28e7c50936c373ca8b9fec
Eugene Merjey <evgeniy.merjey@sysgears.com>
2019-05-30 16:36:51 +0300

    Replaced insert with upsert for GitHubRepositories

--- src/main/scala/repositories/MongoRepository.scala
+++ src/main/scala/repositories/MongoRepository.scala
@@ -3,6 +3,7 @@ package repositories
 import reactivemongo.api.BSONSerializationPack.Writer
 import reactivemongo.api.collections.bson.BSONCollection
 import reactivemongo.api.commands.MultiBulkWriteResult
+import reactivemongo.bson.BSONDocument
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -14,6 +15,20 @@ abstract class MongoRepository[T](reactiveMongo: ReactiveMongo)
 
   protected def insertMany(documents: Seq[T])(implicit writer: Writer[T]): Future[MultiBulkWriteResult] = {
     val collection: Future[BSONCollection] = reactiveMongo.db.map(_.collection(collectionName))
-    collection.flatMap(_.insert.many(documents))
+
+    for {
+      resolvedCollection <- collection
+      updateBuilder <- Future(resolvedCollection.update(ordered = true))
+      updates <- Future.sequence(documents.map(doc => {
+        updateBuilder.element(
+          q = BSONDocument("_id" -> doc.getClass.getDeclaredMethod("_id").invoke(doc).asInstanceOf[String]),
+          u = doc,
+          upsert = true,
+          multi = false
+        )
+      }))
+      result <- updateBuilder.many(updates)
+    } yield result
+
   }
 }
